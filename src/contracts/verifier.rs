4efe17559b6b4e8b1e1c3d44564dfcaec5f0bdbb
hrmk1o3 <20158457+hrmk1o3@users.noreply.github.com>
2023-04-29 23:53:54 +0200

    fix artifacts

--- src/contracts/verifier.rs
+++ src/contracts/verifier.rs
@@ -2,10 +2,5 @@ use ethers::contract::abigen;
 
 abigen!(
     VerifierContract,
-    "./contracts/compiled-artifacts/contracts/SimpleVerifier.sol/SimpleVerifier.json"
-);
-
-abigen!(
-    MerkleTreeContract,
-    "./contracts/compiled-artifacts/contracts/utils/MerkleTree.sol/MerkleTree.json"
+    "./contracts/compiled-artifacts/contracts/Verifier.test.sol/VerifierTest.json"
 );

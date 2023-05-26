2d288a71a0dd7f4e86696f72674d37229f888bc2
AnonymouseTaro <134359388+AnonymouseTaro@users.noreply.github.com>
2023-05-26 18:09:40 +0900

    add test SimpleVerifier (#27)

--- contracts/test/SimpleVerifier.ts
+++ contracts/test/SimpleVerifier.ts
@@ -11,7 +11,7 @@ import {
 describe("SimpleVerifier", function () {
   async function deployVerifier() {
     // Contracts are deployed using the first signer/account by default
-    const [owner] = await ethers.getSigners();
+    const [owner, other] = await ethers.getSigners();
 
     const { recipient } = sampleWitness;
     const networkIndex = recipient;
@@ -19,63 +19,401 @@ describe("SimpleVerifier", function () {
     const Verifier = await ethers.getContractFactory("SimpleVerifierTest");
     const verifier = await Verifier.deploy(networkIndex);
 
-    return { verifier, networkIndex, owner };
+    return { verifier, networkIndex, owner, other };
   }
 
   describe("verifyAsset", function () {
-    it("Should execute without errors", async function () {
-      const { verifier, networkIndex, owner } = await loadFixture(
-        deployVerifier
-      );
+    describe("success", function () {
+      it("Should execute without errors", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
 
-      const {
-        diffTreeInclusionProof,
-        blockHeader,
-        tokenAddress,
-        tokenId,
-        tokenAmount,
-        nonce,
-        recipientMerkleSiblings,
-      } = sampleWitness;
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+        } = sampleWitness;
 
-      const recipient = networkIndex;
-      const asset = {
-        tokenAddress,
-        tokenId,
-        amount: tokenAmount,
-      };
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
 
-      const abiEncoder = new ethers.utils.AbiCoder();
-      const message = abiEncoder.encode(
-        [
-          `${assetStructType}[]`,
-          "bytes32",
-          merkleProofStructType,
-          blockHeaderStructType,
-        ],
-        [[asset], recipient, diffTreeInclusionProof, blockHeader]
-      );
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const message = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [[asset], recipient, diffTreeInclusionProof, blockHeader]
+        );
 
-      const messageBytes = Buffer.from(message.slice(2), "hex");
-      const signature = await owner.signMessage(messageBytes);
+        const messageBytes = Buffer.from(message.slice(2), "hex");
+        const signature = await owner.signMessage(messageBytes);
 
-      const witness = abiEncoder.encode(
-        [
-          `${assetStructType}[]`,
-          "bytes32",
-          merkleProofStructType,
-          blockHeaderStructType,
-          "bytes",
-        ],
-        [[asset], recipient, diffTreeInclusionProof, blockHeader, signature]
-      );
+        const witness = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+            "bytes",
+          ],
+          [[asset], recipient, diffTreeInclusionProof, blockHeader, signature]
+        );
 
-      expect(
-        await verifier.verifyAssets([asset], recipient, witness)
-      ).to.be.equals(true);
-      // expect(await verifier.verifyBlockHash(blockHash, signature)).to.be.equals(
-      //   true
-      // );
+        expect(
+          await verifier.verifyAssets([asset], recipient, witness)
+        ).to.be.equals(true);
+      });
+      it("some assets", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          tokenAddress2,
+          tokenId2,
+          tokenAmount2,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+        const asset2 = {
+          tokenAddress: tokenAddress2,
+          tokenId: tokenId2,
+          amount: tokenAmount2,
+        };
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const message = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader]
+        );
+
+        const messageBytes = Buffer.from(message.slice(2), "hex");
+        const signature = await owner.signMessage(messageBytes);
+
+        const witness = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+            "bytes",
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader, signature]
+        );
+
+        expect(
+          await verifier.verifyAssets([asset, asset2], recipient, witness)
+        ).to.be.equals(true);
+      });
+    });
+    describe("fail", function () {
+      it("illegal signature", async function () {
+        const { verifier, networkIndex, other } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          // nonce,
+          // recipientMerkleSiblings,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const message = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [[asset], recipient, diffTreeInclusionProof, blockHeader]
+        );
+
+        const messageBytes = Buffer.from(message.slice(2), "hex");
+        const signature = await other.signMessage(messageBytes);
+
+        const witness = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+            "bytes",
+          ],
+          [[asset], recipient, diffTreeInclusionProof, blockHeader, signature]
+        );
+        await expect(
+          verifier.verifyAssets([asset], recipient, witness)
+        ).to.be.revertedWith("Fail to verify aggregator's signature.");
+      });
+      it("illegal token address", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          tokenAddress2,
+          tokenAddress3,
+          tokenId2,
+          tokenAmount2,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+        const asset2 = {
+          tokenAddress: tokenAddress2,
+          tokenId: tokenId2,
+          amount: tokenAmount2,
+        };
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const message = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader]
+        );
+
+        const messageBytes = Buffer.from(message.slice(2), "hex");
+        const signature = await owner.signMessage(messageBytes);
+
+        const witness = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+            "bytes",
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader, signature]
+        );
+        const dummyAsset2 = {
+          tokenAddress: tokenAddress3,
+          tokenId: tokenId2,
+          amount: tokenAmount2,
+        };
+        await expect(
+          verifier.verifyAssets([asset, dummyAsset2], recipient, witness)
+        ).to.be.revertedWith("Not same asset: tokenAddress");
+      });
+      it("illegal token id", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          tokenAddress2,
+          tokenId2,
+          tokenId3,
+          tokenAmount2,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+        const asset2 = {
+          tokenAddress: tokenAddress2,
+          tokenId: tokenId2,
+          amount: tokenAmount2,
+        };
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const message = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader]
+        );
+
+        const messageBytes = Buffer.from(message.slice(2), "hex");
+        const signature = await owner.signMessage(messageBytes);
+
+        const witness = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+            "bytes",
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader, signature]
+        );
+        const dummyAsset2 = {
+          tokenAddress: tokenAddress2,
+          tokenId: tokenId3,
+          amount: tokenAmount2,
+        };
+        await expect(
+          verifier.verifyAssets([asset, dummyAsset2], recipient, witness)
+        ).to.be.revertedWith("Not same asset: tokenId");
+      });
+      it("illegal amount", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          tokenAddress2,
+          tokenId2,
+          tokenAmount2,
+          tokenAmount3
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+        const asset2 = {
+          tokenAddress: tokenAddress2,
+          tokenId: tokenId2,
+          amount: tokenAmount2,
+        };
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const message = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader]
+        );
+
+        const messageBytes = Buffer.from(message.slice(2), "hex");
+        const signature = await owner.signMessage(messageBytes);
+
+        const witness = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+            "bytes",
+          ],
+          [[asset, asset2], recipient, diffTreeInclusionProof, blockHeader, signature]
+        );
+        const dummyAsset2 = {
+          tokenAddress: tokenAddress2,
+          tokenId: tokenId2,
+          amount: tokenAmount3,
+        };
+        await expect(
+          verifier.verifyAssets([asset, dummyAsset2], recipient, witness)
+        ).to.be.revertedWith("Not same asset: amount");
+      });
+      it("Not same recipient", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          recipient2
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+
+        const abiEncoder = new ethers.utils.AbiCoder();
+        const message = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [[asset], recipient, diffTreeInclusionProof, blockHeader]
+        );
+
+        const messageBytes = Buffer.from(message.slice(2), "hex");
+        const signature = await owner.signMessage(messageBytes);
+
+        const witness = abiEncoder.encode(
+          [
+            `${assetStructType}[]`,
+            "bytes32",
+            merkleProofStructType,
+            blockHeaderStructType,
+            "bytes",
+          ],
+          [[asset], recipient, diffTreeInclusionProof, blockHeader, signature]
+        );
+        await expect(
+          verifier.verifyAssets([asset], recipient2, witness)
+        ).to.be.revertedWith("Not same recipient");
+      });
     });
   });
 });
+

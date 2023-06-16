b76db0f60389131a6b60cf3d7de77305d343e20a
“AnonymouseTaro” <taro.anonymouse.hogehoge@gmail.com>
2023-06-16 13:02:22 +0900

    add test

--- contracts/test/Verifier.ts
+++ contracts/test/Verifier.ts
@@ -22,50 +22,258 @@ describe("Verifier", function () {
   }
 
   describe("verify", function () {
-    it("Should execute without errors", async function () {
-      const { verifier, networkIndex, owner } = await loadFixture(
-        deployVerifier
-      );
+    describe("success", function () {
+      it("Should execute without errors", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
 
-      const {
-        diffTreeInclusionProof,
-        blockHeader,
-        tokenAddress,
-        tokenId,
-        tokenAmount,
-        nonce,
-        recipientMerkleSiblings,
-      } = sampleWitness;
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          nonce,
+          recipientMerkleSiblings,
+        } = sampleWitness;
 
-      const recipient = networkIndex;
-      const asset = {
-        tokenAddress,
-        tokenId,
-        amount: tokenAmount,
-      };
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
 
-      const messageBytes = Buffer.from(sampleWitness.blockHash.slice(2), "hex");
-      const signature = await owner.signMessage(messageBytes);
-      await verifier.updateTransactionsDigest(blockHeader, signature);
+        const messageBytes = Buffer.from(
+          sampleWitness.blockHash.slice(2),
+          "hex"
+        );
+        const signature = await owner.signMessage(messageBytes);
+        await verifier.updateTransactionsDigest(blockHeader, signature);
 
-      const abiCoder = new ethers.utils.AbiCoder();
-      const witness = abiCoder.encode(
-        ["bytes32", "bytes32[]", merkleProofStructType, blockHeaderStructType],
-        [nonce, recipientMerkleSiblings, diffTreeInclusionProof, blockHeader]
-      );
+        const abiCoder = new ethers.utils.AbiCoder();
+        const witness = abiCoder.encode(
+          [
+            "bytes32",
+            "bytes32[]",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [nonce, recipientMerkleSiblings, diffTreeInclusionProof, blockHeader]
+        );
+        expect(
+          await verifier.verifyAssets([asset], recipient, witness)
+        ).to.be.equals(true);
+      });
+    });
+    describe("fail", function () {
+      it("Transactions digest was not registered", async function () {
+        const { verifier, networkIndex } = await loadFixture(deployVerifier);
 
-      // const witness = await verifier.calcWitness(
-      //   nonce,
-      //   recipientMerkleSiblings,
-      //   diffTreeInclusionProof,
-      //   blockHeader
-      // );
-      expect(
-        await verifier.verifyAssets([asset], recipient, witness)
-      ).to.be.equals(true);
-      // expect(await verifier.verifyBlockHash(blockHash, signature)).to.be.equals(
-      //   true
-      // );
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          nonce,
+          recipientMerkleSiblings,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+
+        const abiCoder = new ethers.utils.AbiCoder();
+        const witness = abiCoder.encode(
+          [
+            "bytes32",
+            "bytes32[]",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [nonce, recipientMerkleSiblings, diffTreeInclusionProof, blockHeader]
+        );
+        await expect(
+          verifier.verifyAssets([asset], recipient, witness)
+        ).to.be.revertedWith("Transactions digest was not registered");
+      });
+      it("Only one type of asset is available", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          nonce,
+          recipientMerkleSiblings,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+
+        const messageBytes = Buffer.from(
+          sampleWitness.blockHash.slice(2),
+          "hex"
+        );
+        const signature = await owner.signMessage(messageBytes);
+        await verifier.updateTransactionsDigest(blockHeader, signature);
+
+        const abiCoder = new ethers.utils.AbiCoder();
+        const witness = abiCoder.encode(
+          [
+            "bytes32",
+            "bytes32[]",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [nonce, recipientMerkleSiblings, diffTreeInclusionProof, blockHeader]
+        );
+        await expect(
+          verifier.verifyAssets([asset, asset], recipient, witness)
+        ).to.be.revertedWith("Only one type of asset is available");
+      });
+      it("Fail to verify transaction hash", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          nonce,
+          recipientMerkleSiblings,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+
+        const messageBytes = Buffer.from(
+          sampleWitness.blockHash.slice(2),
+          "hex"
+        );
+        const signature = await owner.signMessage(messageBytes);
+        await verifier.updateTransactionsDigest(blockHeader, signature);
+
+        const abiCoder = new ethers.utils.AbiCoder();
+        const witness = abiCoder.encode(
+          [
+            "bytes32",
+            "bytes32[]",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [
+            nonce,
+            recipientMerkleSiblings,
+            {
+              ...diffTreeInclusionProof,
+              value:
+                "0x0eb318137a57bc2291eb5dbcbe8b10fc3d46e45933c2e14e9030880b380bac98",
+            },
+            blockHeader,
+          ]
+        );
+        await expect(
+          verifier.verifyAssets([asset], recipient, witness)
+        ).to.be.revertedWith("Fail to verify transaction hash");
+      });
+      it("Fail to verify transactions digest", async function () {
+        const { verifier, networkIndex, owner } = await loadFixture(
+          deployVerifier
+        );
+
+        const {
+          diffTreeInclusionProof,
+          blockHeader,
+          tokenAddress,
+          tokenId,
+          tokenAmount,
+          nonce,
+          recipientMerkleSiblings,
+        } = sampleWitness;
+
+        const recipient = networkIndex;
+        const asset = {
+          tokenAddress,
+          tokenId,
+          amount: tokenAmount,
+        };
+
+        const messageBytes = Buffer.from(
+          sampleWitness.blockHash.slice(2),
+          "hex"
+        );
+        const signature = await owner.signMessage(messageBytes);
+        await verifier.updateTransactionsDigest(blockHeader, signature);
+
+        const abiCoder = new ethers.utils.AbiCoder();
+        const witness = abiCoder.encode(
+          [
+            "bytes32",
+            "bytes32[]",
+            merkleProofStructType,
+            blockHeaderStructType,
+          ],
+          [
+            nonce,
+            recipientMerkleSiblings,
+            {
+              ...diffTreeInclusionProof,
+              siblings: [
+                "0xc71603f33a1144ca7953db0ab48808f4c4055e3364a246c33c18a9786cb0b359",
+                "0xc71603f33a1144ca7953db0ab48808f4c4055e3364a246c33c18a9786cb0b359",
+                "0xc71603f33a1144ca7953db0ab48808f4c4055e3364a246c33c18a9786cb0b359",
+                "0xc71603f33a1144ca7953db0ab48808f4c4055e3364a246c33c18a9786cb0b359",
+              ],
+            },
+            blockHeader,
+          ]
+        );
+        await expect(
+          verifier.verifyAssets([asset], recipient, witness)
+        ).to.be.revertedWith("Fail to verify transactions digest");
+      });
+      it("Fail to verify aggregator's signature.", async function () {
+        const { verifier, owner } = await loadFixture(deployVerifier);
+
+        const { blockHeader } = sampleWitness;
+
+        const messageBytes = Buffer.from(
+          sampleWitness.blockHash.slice(2),
+          "hex"
+        );
+        const signature = await owner.signMessage(messageBytes);
+        await expect(
+          verifier.updateTransactionsDigest(
+            {
+              ...blockHeader,
+              transactionsDigest:
+                "0x0eb318137a57bc2291eb5dbcbe8b10fc3d46e45933c2e14e9030880b380bac98",
+            },
+            signature
+          )
+        ).to.be.revertedWith("Fail to verify aggregator's signature.");
+      });
     });
   });
 });
